a5f4602d95f57cd70a265ce9c173b41256639d62
batuhanyrs <batuhanyaras@yahoo.com>
2024-08-06 15:40:35 +0300

    assetsUpdated

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Expense Tracker',
       theme: ThemeData(buttonTheme: const ButtonThemeData()).copyWith(
         appBarTheme: allAppBar(),
